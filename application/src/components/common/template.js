c2278420d02aead8302558161f20227afc852bfa
Luis Perez <44172765+Lap343@users.noreply.github.com>
2021-09-17 13:46:15 -0700

    feat(user-display): add user above the nav bar and fix the routing issue

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Nav } from '../../components';
 import './template.css';
 
 const Template = props => {
+    
+    const user = useSelector(state => state.auth.email);
+
     return (
         <div className="bg-layer">
             <div className="fg-layer">
                 <label className="logo">Luigi's Pizzeria</label>
+                {user && <div>Hello {user}</div>}
                 <Nav />
                 {props.children}
             </div>
--- application/src/components/login/login-form/loginForm.js
+++ application/src/components/login/login-form/loginForm.js
@@ -1,48 +1,59 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'; 
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'; 
+import { useHistory } from 'react-router';
 import { loginUser } from '../../../redux/actions/authActions'
 
-const mapActionsToProps = dispatch => ({
-  commenceLogin(email, password) {
-    dispatch(loginUser(email, password))
-  }
-})
+const LoginForm = (props) => {
 
-class LoginForm extends Component {
-  state = {
-    email: "",
-    password: "",
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  let history = useHistory();
 
-  login(e) {
-    if(this.state.email !== '' && this.state.password !== ''){
+  const login = (e) => {
+    if(email !== '' && password !== ''){
       e.preventDefault();
-      this.props.commenceLogin(this.state.email, this.state.password);
-      this.props.onLogin();
+      dispatch(loginUser(email, password));
+      history.push("/view-orders");
     }
   }
 
-  onChange(key, val) {
-    this.setState({ [key]: val });
+  const onChange = (key, val) => {
+    if(key === "email"){
+      setEmail(val);
+    }else if(key === "password"){
+      setPassword(val);
+    }
   }
 
-  render() {
-    return (
-      <form>
-        <div className="form-group">
-          <label htmlFor="inputEmail">Email</label>
-          <input type="text" className="form-control" id="inputEmail" placeholder="test@test.com" value={this.state.email} onChange={e => this.onChange('email', e.target.value)}></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputPassword">Password</label>
-          <input type="password" className="form-control" id="inputPassword" value={this.state.password} onChange={e => this.onChange('password', e.target.value)}></input>
-        </div>
-        <div className="d-flex justify-content-center">
-            <button onClick={e => this.login(e)} type="submit" className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="inputEmail">Email</label>
+        <input 
+          type="text" 
+          className="form-control" 
+          id="inputEmail" 
+          placeholder="test@test.com" 
+          value={email} 
+          onChange={e => onChange('email', e.target.value)}
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="inputPassword">Password</label>
+        <input 
+          type="password" 
+          className="form-control" 
+          id="inputPassword" 
+          value={password} 
+          onChange={e => onChange('password', e.target.value)}
+        ></input>
+      </div>
+      <div className="d-flex justify-content-center">
+          <button onClick={e => login(e)} type="submit" className="btn btn-primary">Login</button>
+      </div>
+    </form>
+  );
 }
 
-export default connect(null, mapActionsToProps)(LoginForm);
+export default LoginForm;
